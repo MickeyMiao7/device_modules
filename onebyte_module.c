c5c1d9da402c8d2bd758cea07887edcc0989787c
mickey <327191467@qq.com>
2017-03-21 23:23:23 -0700

    add read/write function

--- onebyte_module.c
+++ onebyte_module.c
@@ -40,13 +40,28 @@ int onebyte_release(struct inode *inode, struct file *filep)
 
 ssize_t onebyte_read(struct file *filep, char *buf, size_t
 		count, loff_t *f_pos)
-{    /*please complete the function on your own*/
-
+{    
+     if (*f_pos == 0 ){
+	copy_to_user(buf, onebyte_data, 1);
+	*f_pos += 1;
+	return 1;
+     }
+     else{
+	return 0;
+     }
 }
 
 ssize_t onebyte_write(struct file *filep, const char *buf, size_t count, loff_t *f_pos)
 {
-     /*please complete the function on your own*/
+     if (*f_pos == 0 ){
+	copy_from_user(onebyte_data, buf, 1);
+	*f_pos += 1;
+	return 1;
+     }
+     else{
+	return -ENOSPC;
+     }
+          
 }
 static int onebyte_init(void)
 {
